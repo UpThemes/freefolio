d91936cabbd1f0b690179b539c00e7193db727c8
Chris Wallace <chris@liftux.com>
2014-08-21 15:01:35 -0500

    Updated plugin info.

--- freefolio.php
+++ freefolio.php
@@ -1,16 +1,33 @@
 <?php
 /**
- * Plugin Name: Freefolio
- * Plugin URI: http://wordpress.org/plugins/freefolio/
- * Description: Adds a portfolio post type, custom widget and automatically imports Dribbble shots.
- * Author: Matthew Simo
- * Version: 1.0.0
- * Author URI: https://upthemes.com/
- * License: GPL2+
- * Text Domain: freefolio
- * Domain Path: /languages/
+ * Freefolio
+ *
+ * Adds a portfolio post type, custom widget and automatically imports Dribbble shots.
+ *
+ * @package   Freefolio
+ * @author    Matthew Simo <matthew@upthemes.com>
+ * @license   GPL-2.0+
+ * @link      https://upthemes.com
+ * @copyright 2014 UpThemes
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Freefolio
+ * Plugin URI:        http://wordpress.org/plugins/freefolio-by-upthemes/
+ * Description:       Adds a portfolio post type, custom widget and automatically imports Dribbble shots.
+ * Version:           1.0.0
+ * Author:            Matthew Simo and Chris Wallace
+ * Author URI:        https://upthemes.com
+ * Text Domain:       freefolio
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/UpThemes/freefolio
  */
 
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
 
 define( 'DPI__VERSION',       '1.0.0' );
 define( 'DPI__PLUGIN_DIR',    plugin_dir_path( __FILE__ ) );
